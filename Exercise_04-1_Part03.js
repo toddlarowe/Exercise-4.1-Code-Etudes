89b1709d3b64e2500b8b6f3af6fc64a7cc59e8d4
toddlarowe <32879848+toddlarowe@users.noreply.github.com>
2017-10-17 12:26:57 -0700

    Ex 4.1 Code Etudes w/Part03

--- Exercise_04-1_Part03.js
+++ Exercise_04-1_Part03.js
@@ -1,2 +1,19 @@
 //Exercise 4.1 PART 3
 //Given an array of objects with "first_name" and "last_name" fields, write a callback for the ".sort" function to sort the names alphabetically by last name. If two last names are the same, then sort those by first name.
+
+let contacts = [];
+  contacts.push(["Evans", "Bill"]);
+  contacts.push(["Marsalis", "Wynton"]);
+  contacts.push(["Blakey", "Art"]);
+  contacts.push(["Marsalis", "Branford"]);
+
+contacts.sort(function(a, b){
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+    return a.contacts.localeCompare(b.contacts);
+})
+
+console.log(contacts)
+
+//Holy crap, I think I made it work correctly...
